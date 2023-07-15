ab0e5d4e0e7732a06fd821eef763a213a09fc6d8
Alicia <alicia.sudlerd@ut.ee>
2023-07-15 16:01:53 +0300

    complete task 2

---
+++ Templates/templates/Task2/task2.h
@@ -0,0 +1,17 @@
+/*Write a function template that can swap two variables of any type. (Difficulty: 1)*/
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T, typename U>
+void swap(T& a, U& b) {
+    T temp = a;
+    a = static_cast<T>(b);
+    b = static_cast<U>(temp);
+}
